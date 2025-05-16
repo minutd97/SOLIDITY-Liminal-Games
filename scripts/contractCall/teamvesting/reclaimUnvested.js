1a3396f6fd0a11e6eb78b58f10ea6e74986c8a00
DESKTOP-1658P1T\offset <minutd97@gmail.com>
2025-05-16 21:55:49 +0300

    Update 12.5.1

---
+++ scripts/contractCall/teamvesting/reclaimUnvested.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const { ethers } = require("hardhat");
+const path = require("path");
+const {
+    getOwner,
+    getProvider,
+    sendTx,
+    setTxLogging,
+    log_TokenBalance,
+    log_EthBalance
+} = require(path.resolve(process.cwd(), "scripts/deployUtils"));
+const {TEAM_VESTING_CONTROLLER, LIMINAL_TOKEN} = require(path.resolve(process.cwd(), "scripts/deployAddresses"));
+
+async function execute() {
+    try {
+        setTxLogging(true);
+        const provider = getProvider();
+        const owner = new ethers.Wallet(getOwner(), provider);
+        const TeamVestingController = await ethers.getContractAt("TeamVestingController", TEAM_VESTING_CONTROLLER, owner);
+    
+        const beneficiary = "0xD580273B481c6acb42eB979DF6a369eB657B1CE9";
+        await sendTx(TeamVestingController.connect(owner).reclaimUnvestedERC20(beneficiary, LIMINAL_TOKEN), 
+            `Reclaiming unvested LIM tokens from : ${beneficiary}`);
+
+        console.log("✅ Execution Succeded !");
+        process.exit(0);
+    } catch (error) {
+        console.error("❌ Execution failed:", error);
+        process.exit(1);
+    }
+}
+
+execute();
